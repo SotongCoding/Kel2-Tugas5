c23eb1812b314cc82ec6c2e823f11bd3359b1b1a
Harryanto <harry@hwinsen.com>
2022-08-24 11:14:16 +0800

    Add spawn function and cooldown

--- Assets/Scripts/Module-UnitWeaponSpawner/UnitWeaponSpawner.cs
+++ Assets/Scripts/Module-UnitWeaponSpawner/UnitWeaponSpawner.cs
@@ -1,3 +1,4 @@
+using Agate.MVC.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,8 +13,23 @@ namespace TankU.UnitWeaponSpawner
         [SerializeField] private GameObject _bomb;
         [SerializeField] private int _bulletAmountToPool;
         [SerializeField] private int _bombAmountToPool;
+        private float _spawnBulletCooldownRunning;
+        private float _spawnBombCooldownRunning;
+        [SerializeField] private float _spawnBulletCooldownDuration;
+        [SerializeField] private float _spawnBombCooldownDuration;
 
-        void Start()
+        private void Awake()
+        {
+            PublishSubscribe.Instance.Subscribe<SpawnBulletMessage>(SpawnBullet);
+            PublishSubscribe.Instance.Subscribe<SpawnBombMessage>(SpawnBomb);
+        }
+        private void OnDestroy()
+        {
+            PublishSubscribe.Instance.Unsubscribe<SpawnBulletMessage>(SpawnBullet);
+            PublishSubscribe.Instance.Unsubscribe<SpawnBombMessage>(SpawnBomb);
+        }
+
+        private void Start()
         {
             _pooledBullet = new List<GameObject>();
             _pooledBomb = new List<GameObject>();
@@ -21,6 +37,10 @@ namespace TankU.UnitWeaponSpawner
             _bomb = Resources.Load<GameObject>(@"Prefabs/Bomb");
             _bulletAmountToPool = 10;
             _bombAmountToPool = 10;
+            _spawnBulletCooldownDuration = 2f;
+            _spawnBombCooldownDuration = 2f;
+            _spawnBulletCooldownRunning = _spawnBulletCooldownDuration;
+            _spawnBombCooldownRunning = _spawnBombCooldownDuration;
 
             for (int i = 0; i < _bulletAmountToPool; i++)
             {
@@ -39,7 +59,19 @@ namespace TankU.UnitWeaponSpawner
             }
         }
 
-        public GameObject GetPooledBullet()
+        private void Update()
+        {
+            if (_spawnBulletCooldownRunning < _spawnBulletCooldownDuration)
+            {
+                _spawnBulletCooldownRunning += Time.deltaTime;
+            }
+            if (_spawnBombCooldownRunning < _spawnBombCooldownDuration)
+            {
+                _spawnBombCooldownRunning += Time.deltaTime;
+            }
+        }
+
+        private GameObject GetPooledBullet()
         {
             for (int i = 0; i < _bulletAmountToPool; i++)
             {
@@ -51,16 +83,36 @@ namespace TankU.UnitWeaponSpawner
             return null;
         }
 
-        public GameObject GetPooledBomb()
+        private GameObject GetPooledBomb()
         {
-            for (int i = 0; i < _bulletAmountToPool; i++)
+            for (int i = 0; i < _bombAmountToPool; i++)
             {
-                if (!_pooledBullet[i].activeInHierarchy)
+                if (!_pooledBomb[i].activeInHierarchy)
                 {
-                    return _pooledBullet[i];
+                    return _pooledBomb[i];
                 }
             }
             return null;
+        }
+
+        private void SpawnBullet(SpawnBulletMessage message)
+        {
+            if (_spawnBulletCooldownRunning >= _spawnBulletCooldownDuration)
+            {
+                GameObject _bulletToSpawn = GetPooledBullet();
+                _bulletToSpawn.SetActive(true);
+                _spawnBulletCooldownRunning = 0;
+            }
+        }
+
+        private void SpawnBomb(SpawnBombMessage message)
+        {
+            if (_spawnBombCooldownRunning >= _spawnBombCooldownDuration)
+            {
+                GameObject _bombToSpawn = GetPooledBomb();
+                _bombToSpawn.SetActive(true);
+                _spawnBombCooldownRunning = 0;
+            }
         }
     }
 }
