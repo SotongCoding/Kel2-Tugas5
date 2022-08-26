cf1d41017fdaab09de3453a2d6ba279676c94cec
XAlpha9 <theoalpha9@gmail.com>
2022-08-26 13:59:19 +0800

    not done yet

--- Assets/Scripts/Module-GameplayUI/GameplayUI.cs
+++ Assets/Scripts/Module-GameplayUI/GameplayUI.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Agate.MVC.Core;
 using TankU.GameStatus;
+using TankU.Unit.UnitStatus;
 using TMPro;
+using UnityEngine.UI;
 namespace TankU.GameplayUI
 {
     public class GameplayUI : MonoBehaviour
@@ -12,13 +14,23 @@ namespace TankU.GameplayUI
         private TextMeshProUGUI TimerTxT;
         private GameStatus.TimerGameplay _TimerGameplay;
         [SerializeField]
+        private Unit.UnitStatus.UnitStatusControl _HP;
+        [SerializeField]
         private GameObject[] BombP1, BombP2, BombP3, BombP4;
         [SerializeField]
         private int[] BombLeft;
+        [SerializeField]
+        private Slider[] Health;
         // Start is called before the first frame update
         void Start()
         {
             _TimerGameplay = GameObject.Find("GameStatus").GetComponent<TimerGameplay>();
+            _HP = GetComponent<UnitStatusControl>();
+            Health = new Slider[2];
+            Health[0] = GameObject.Find("P1-Hp").GetComponent<Slider>();
+            Health[1] = GameObject.Find("P2-Hp").GetComponent<Slider>();
+            Health[0].value = _HP._unitHealth;
+            Health[1].value = _HP._unitHealth;
             BombLeft = new int[4];
             BombLeft[0] = 5;
             BombLeft[1] = 5;
@@ -36,6 +48,17 @@ namespace TankU.GameplayUI
             TimerTxT.text = _TimerGameplay.timer.ToString();
         }
 
+        public void DisplayP1Health()
+        {
+            Health[0].value = _HP._unitHealth;
+            
+        }
+        public void DisplayP2Health()
+        {
+            Health[1].value = _HP._unitHealth;
+
+        }
+        
         void ReduceBomb(MessageSpawnBomb message)
         {
             if (message.PlayerId == 1)
@@ -51,12 +74,12 @@ namespace TankU.GameplayUI
             else if (message.PlayerId == 3)
             {
                 BombLeft[2]--;
-                BombP2[BombLeft[2]].SetActive(false);
+                BombP3[BombLeft[2]].SetActive(false);
             }
             else if (message.PlayerId == 4)
             {
                 BombLeft[3]--;
-                BombP2[BombLeft[3]].SetActive(false);
+                BombP4[BombLeft[3]].SetActive(false);
             }
         }
         
